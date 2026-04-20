962f11575b608d2cf05fe7ea18152f7a48774415
Sangita Jankar <sangitajankar11@gmail.com>
2026-04-20 21:05:16 +0530

    myfirst JS program with variables

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("This is my first javascript program..");
+console.log("This is my second line of code..");
+
+function myFunction() {
+    alert("Do you want to submit the form?..");
+}
+
+let emailaddress = "john@gmail.com";
+const pi = 3.14;
+
+console.log("The value of pi is: " + pi);
+console.log("The email address is: " + emailaddress);
+
+emailaddress = "test@test.com";
+console.log("The updated email address is: " + emailaddress);
+
+//try to change const and print it
+// pi = 3.14159; // This will cause an error because pi is a constant
+// console.log("The updated value of pi is: " + pi); 
